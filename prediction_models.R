7584403eade72bb115830a943dda9967c455743c
Eliska Kopecka <eli.kopeck@gmail.com>
2019-12-01 02:39:06 +0100

    Test structure done, Lin regression model created

---
+++ prediction_models.R
@@ -0,0 +1,21 @@
+get_linear_regression_model <- function(force_new_model = FALSE) {
+  model_location <- "./regression_model.rds"
+  
+  if(!file.exists(model_location) || force_new_model) {
+    regression_model <-glm(sentiment ~ ., data.train, family = binomial)
+    saveRDS(regression_model, model_location)
+    return (regression_model)
+  }
+  regression_model <- readRDS(model_location)
+  return (regression_model)
+}
+
+get_naive_bays <- function (force_new_model = FALSE) {
+  model_location <- "./naive_bayes.rds"
+  
+  if(!file.exists(model_location) || force_new_model) {
+    library(e1071)
+    nb_model <- naiveBayes(sentiment ~., data = data.train)
+    saveRDS(nb_model, model_location)
+  }
+}
--- text_mining.R
+++ text_mining.R
@@ -7,6 +7,16 @@ library(SnowballC)
 
 source("data_loader.R")
 source("data_cleaner.R")
+source("prediction_models.R")
+
+measure_accuracy <- function(results, predictions) {
+  # Construct confusion matrix
+  table(results, predictions)
+  # Accuracy 
+  accuracy <-  sum(predictions == results)/length(results)
+  accuracy
+  return (accuracy)
+}
 
 # Load data
 data <- load_data()
@@ -18,4 +28,22 @@ data <- clean_data(data)
 split <- sample.split(data$sentiment, SplitRatio = 0.75)
 data.train <- subset(data, split == TRUE)
 data.test <- subset(data, split == FALSE)
+
+# building models
+
+# logistic regression -  82.7% accuracy
+regression_model <- get_linear_regression_model()
+
+# Predict and measure accuracy
+pred <- predict(regression_model,weighting = function(x) weightTfIdf(x, normalize =TRUE), data.test, type = "response")
+pred <- (pred>=0.5)
+measure_accuracy(data.test$sentiment, pred)
+
+# Measure accuracy for training data
+train_predictions <- predict(regression_model, data.train, type = "response")
+train_predictions <- (data.train$pred>=0.5)
+measure_accuracy(data.train$sentiment, train_predictions)
+
+
+
 
